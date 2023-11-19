93a38d28ba5eabc3f92ea4c8d8469b7dd7bfd7c3
Facundo <facundo.stazione@gmail.com>
2023-11-19 17:18:20 -0300

    intento agregar js

---
+++ main.js
@@ -0,0 +1,9 @@
+let btnAgregarCarrito = document.getElementByClassName("btnAgregarCarrito");
+
+btnAgregarCarrito.addEventListener("click", () => {
+    Swal.fire({
+        title: "The Internet?",
+        text: "That thing is still around?",
+        icon: "question"
+      });
+})
